adf4532849b35771c4693bfac87b824ca1c33e5c
ed_ <eddieweks@gmail.com>
2024-01-24 00:56:05 +0300

    updates

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Repr. a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        """
+        # private attr. size
+        self.__size = size
