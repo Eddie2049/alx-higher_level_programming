7ba1e80d473d5ab3aa4325f26820e8ad1778d87e
ed_ <eddieweks@gmail.com>
2024-04-10 04:46:36 +0300

    updates

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
+  // function that converts a no. from base 10 to another base passed as argument.
   return function (num) {
     return num.toString(base);
   };
