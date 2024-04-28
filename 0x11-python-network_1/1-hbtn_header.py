146bb91943d46bff21f54f1c6895a866f3d5d107
ed_ <eddieweks@gmail.com>
2024-04-28 19:56:26 +0300

    updates

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL, 
-sends a request to the URL and displays the value 
+a Python script that takes in a URL,
+sends a request to the URL and displays the value
 of the X-Request-Id variable found in the header of the response.
 """
 
 import urllib.request as req_
 import sys
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
