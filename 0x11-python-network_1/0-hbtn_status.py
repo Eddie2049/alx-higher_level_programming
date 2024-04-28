80508bb048d054d847d5bc877cae2fe717af694a
ed_ <eddieweks@gmail.com>
2024-04-28 15:10:28 +0300

    updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ with req_.urlopen(url_) as response:
     res_ = response.read().decode('utf-8')
     utf8_content = res_.decode('utf-8')
 
-print("res_ response:")
+print("Body response:")
 print("\t- type:", type(res_))
 print("\t- content:", res_)
 print("\t- utf8 content:", utf8_content)
