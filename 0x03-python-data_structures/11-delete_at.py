636777fa5815c7c412360e747548f3167841d287
ed_ <eddieweks@gmail.com>
2024-09-18 02:03:34 +0300

    updates

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    ''' function checks if the index is valid
+    (not negative and within the range of the list)
+    '''
+    if idx_ < 0 or idx_ >= len(my_list):
+        return my_list
+
+    # remove the element at the specified index by slicing the list
+    del my_list[idx]
+
+    return my_list
